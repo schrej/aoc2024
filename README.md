c46030b7d55be6092d2bab74f7fdff8c6e2abb63
Jakob Schrettenbrunner <jakob.schrettenbrunner@telekom.de>
2023-12-01 09:55:17 +0100

    initial commit

---
+++ README.md
@@ -0,0 +1,5 @@
+# aoc2023
+
+Rust, maybe I'll learn it this time.
+
+Uses the awesome [cargo-aoc](https://github.com/gobanos/cargo-aoc).
---
+++ src/_template.rs
@@ -0,0 +1,19 @@
+use aoc_runner_derive::{aoc, aoc_generator};
+use std::num::ParseIntError;
+
+#[aoc_generator(day1)]
+fn parse_input_day1(input: &str) -> Result<INPUT, ParseIntError> {
+}
+
+#[aoc(day1, part1)]
+fn solve_part1(depths: INPUT) -> () {
+}
+
+#[aoc(day1, part2)]
+fn solve_part2(depths: INPUT) -> () {
+}
+
+#[cfg(test)]
+mod tests {
+    use super;
+}
